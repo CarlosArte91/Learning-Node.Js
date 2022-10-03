b67cc6fb11854b51cffc890b7eb9593ed2e57ac3
Carlos Cruz <carturo99@gmail.com>
2022-10-02 23:31:10 -0500

    first commit

---
+++ basico/modules/saludo.js
@@ -0,0 +1,12 @@
+const saludar = (nombre) => {
+    return `¿Hola amiguito ${nombre} como estás?`;
+};
+
+const saludarHolaMundo = () => {
+    return "Hola mundo";
+};
+
+module.exports = {
+    saludar,
+    saludarHolaMundo
+};
